2d0223f82e8fb6fcf655d822f8fabfe7a70fce99
Boris Muñoz <bmunoz89@users.noreply.github.com>
2020-06-07 23:56:58 -0400

    add mongodb

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,54 +1,64 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
-import { ProductModel } from 'src/products/products.model'
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import {
+  DeleteQueryResult,
+  UpdateQueryResult,
+} from 'src/interfaces/mongo.interface'
 import {
   ProductBodyDTO,
   ProductIdDTO,
   ProductPartialBodyDTO,
 } from './dto/products.dto'
+import { Product, ProductI } from './schemas/product.schema'
 
 @Injectable()
 export class ProductsService {
-  products: ProductModel[] = [
-    {
-      id: '2020-05-30T22:50:58.393Z',
-      title: '2123',
-      description: 'Description to be tested',
-      price: 1,
-    },
-  ]
+  constructor(
+    @InjectModel(Product.name) private readonly productModel: Model<Product>,
+  ) {}
 
-  insert(product: ProductBodyDTO): string {
-    const id = new Date().toISOString()
-    const newProduct: ProductModel = new ProductModel(
-      id,
-      product.title,
-      product.description,
-      product.price,
+  async create(productBodyDTO: ProductBodyDTO): Promise<ProductI> {
+    const product = await this.productModel.create(productBodyDTO)
+    return product.toJSON()
+  }
+
+  async getAll(): Promise<ProductI[]> {
+    return await this.productModel
+      .find()
+      .sort('-createdAt')
+      .lean()
+  }
+
+  async getOne(id: ProductIdDTO['id']): Promise<ProductI | null> {
+    return await this.productModel.findById(id).lean()
+  }
+
+  async update(
+    id: ProductIdDTO['id'],
+    fieldsToUpdate: ProductBodyDTO,
+  ): Promise<UpdateQueryResult> {
+    return await this.productModel.updateOne(
+      {
+        _id: id,
+      },
+      fieldsToUpdate,
     )
-    this.products.push(newProduct)
-    return id
   }
 
-  all(): ProductModel[] {
-    return this.products.reverse()
-  }
-
-  get(id: ProductIdDTO['id']): ProductModel {
-    const product: ProductModel | undefined = this.products.find(
-      product => product.id === id,
-    )
-    if (product === undefined) throw new NotFoundException()
-    return product
-  }
-
-  update(id: ProductIdDTO['id'], fieldsToUpdate: ProductBodyDTO): void {
-    Object.assign(this.get(id), fieldsToUpdate)
-  }
-
-  updatePartial(
+  async updatePartial(
     id: ProductIdDTO['id'],
     fieldsToUpdate: ProductPartialBodyDTO,
-  ): void {
-    Object.assign(this.get(id), fieldsToUpdate)
+  ): Promise<UpdateQueryResult> {
+    return await this.productModel.updateOne(
+      {
+        _id: id,
+      },
+      fieldsToUpdate,
+    )
+  }
+
+  async delete(id: ProductIdDTO['id']): Promise<DeleteQueryResult> {
+    return await this.productModel.deleteOne({ _id: id })
   }
 }
---
+++ src/products/schemas/product.schema.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema as DSchema, SchemaFactory } from '@nestjs/mongoose'
+import { Document, Schema, Types } from 'mongoose'
+
+export interface ProductI {
+  _id: string
+  title: string
+  description: string
+  price: number
+  created_at: Date
+  updated_at: Date
+}
+
+@DSchema({
+  versionKey: false,
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+})
+export class Product extends Document {
+  _id!: Types.ObjectId
+
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  title: string
+
+  @Prop({
+    required: true,
+    trim: true,
+  })
+  description: string
+
+  @Prop({
+    required: true,
+  })
+  price: number
+
+  @Prop()
+  readonly createdAt?: Date
+
+  @Prop()
+  readonly updatedAt?: Date
+}
+
+export const ProductSchema: Schema<Product> = SchemaFactory.createForClass(
+  Product,
+)
