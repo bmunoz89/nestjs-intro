550bbdbd49fd85b4be5ea13489725b82b85bfd59
Boris Muñoz <bmunoz89@users.noreply.github.com>
2020-06-11 19:44:55 -0400

    add request logger

---
+++ src/logger.middleware.ts
@@ -0,0 +1,30 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
+import { Request, Response } from 'express'
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger: Logger = new Logger('Request')
+
+  use(req: Request, res: Response, next: Function) {
+    const requestStart = Date.now()
+
+    res.on('finish', () => {
+      const url = `${req.baseUrl}${req.url}`
+      const body = JSON.stringify(req.body)
+      const protocol = `${req.protocol.toUpperCase()}/${req.httpVersion}`
+      const elapsed = `${Date.now() - requestStart}ms`
+      const message = [
+        req.method,
+        url,
+        body,
+        protocol,
+        res.statusCode,
+        req.headers['content-length'] || 0,
+        elapsed,
+      ]
+      this.logger.log(message.join(' '))
+    })
+
+    next()
+  }
+}
